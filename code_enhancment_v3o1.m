67289e303bae5e36ba56c744cd83c0fd94587008
Nibedita Chakraborty <96379756+NiviRocks@users.noreply.github.com>
2023-02-18 14:26:08 +0530

    functions

--- code_enhancment_v3o1.m
+++ code_enhancment_v3o1.m
@@ -61,3 +61,82 @@ writelines(s1,"v3_1_text_output.txt",WriteMode="append");
 writelines(s2,"v3_1_text_output.txt",WriteMode="append"); 
 writelines(s3,"v3_1_text_output.txt",WriteMode="append");
 writelines(s4,"v3_1_text_output.txt",WriteMode="append"); 
+
+%function main
+function [new_IMG]=main(IMG,r,c,Vmin,t)
+    new_IMG=zeros(r,c);
+    for x = 1:r
+        for y = 1:c
+            if ~new_IMG(x,y) %if pixel not marked water
+                %fprintf("x %d y %d\n",x,y);
+                if IMG(x,y)>=Vmin && IMG(x,y)<=t+Vmin %check within threshold
+                    [new_t]=threshold_shift(IMG,r,c,x,y,t,Vmin); %get new threshold
+                    if new_t ~=-1 % new_t == -1 then pixel is not water
+                        %fprintf("new t %d\n",new_t);
+                        J1 = regiongrown(IMG,x,y,new_t); %using new local threshold
+                        new_IMG=new_IMG+J1;
+                        if mod(x,5)==0 && mod(y,200)==0
+                            figure, imshow(imadjust(new_IMG));
+                        end
+                    end
+                    %fprintf("out\n");
+                end
+            end
+        end
+    end
+    new_IMG(new_IMG>0)=255;
+    % non zero pixel - not water (255 - white)
+end
+
+% function to check if pixel is water
+function [result,mean] = isWater(IMG,r,c,x,y,t,Vmin)
+    count=0; mean=0;
+    for j = -2:2 % window row
+        for k = -2:2 % window col            
+            if (x+j)>1 && (x+j)<r && (y+k)>1 && (y+k)<c % if pixel within image
+                pxl_value=IMG(x+j,y+k);
+                if pxl_value>=Vmin && pxl_value<=t+Vmin  % if pixel value in [Vmin,Vmin+t]
+                    count = count+1; % count water pixels
+                    mean=mean+pxl_value;
+                end
+            end
+        end
+    end
+    mean=mean/count;
+    if count>=12 % if water pixel more than 50% in window
+        result = 1; % true
+    else 
+        result = 0; % false
+    end
+end 
+
+% function for threshold shifting
+function [new_t] = threshold_shift(IMG,r,c,x,y,t,Vmin)
+    new_t=t+1;
+    [isw1,m1]=isWater(IMG,r,c,x,y,t,Vmin); %mean for global threshold
+    [isw2,m2]=isWater(IMG,r,c,x,y,new_t,Vmin); %mean for t1'
+    if isw1==0 && isw2==0 %if pixel not water return -1
+        new_t=-1;
+    else
+        %fprintf("IN ... x %d y %d md %f\n",x,y,abs(m1-m2));
+        while abs(m1-m2)>=0.1 && isw1 && isw2 
+            m1=m2; isw1=isw2;
+            new_t=new_t+1;
+            [isw2,m2]=isWater(IMG,r,c,x,y,new_t,Vmin);
+            fprintf("m1-m2: %f \n",abs(m1-m2));
+        end
+        fprintf("x %d y %d new t %f\n",x,y,new_t);
+    end
+end
+
+%function for area of inundated image
+function [area]=findArea(IMG,r,c)
+    area=0;
+    for x=1:r
+        for y=1:c
+            if IMG(x,y)
+                area=area+100;
+            end
+        end
+    end
+end
