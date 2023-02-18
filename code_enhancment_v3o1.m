8e9a72a07e18fa9583226da4d7b62d9863ced99b
Nibedita Chakraborty <96379756+NiviRocks@users.noreply.github.com>
2023-02-18 14:25:43 +0530

    image display and saving

--- code_enhancment_v3o1.m
+++ code_enhancment_v3o1.m
@@ -14,3 +14,50 @@ t_pre=std(IMG_pre,0,"all")/2;
 %find minimum pixel value
 Vmin_post = min(IMG_post,[],"all");
 Vmin_pre = min(IMG_pre,[],"all");
+figure, imshow(imadjust(uint8(IMG_pre)));
+figure, imshow(imadjust(uint8(IMG_post)));
+
+fprintf("---------pre---------\nPre Vmin: %f Pre t: %f\n------------------------\n",Vmin_pre,t_pre);
+new_IMG_pre=main(IMG_pre,r,c,Vmin_pre,t_pre);
+fprintf("---------post----------\nPost Vmin: %f Post t: %f\n------------------------\n",Vmin_post,t_post);
+new_IMG_post=main(IMG_post,r,c,Vmin_post,t_post);
+fprintf("Done\n");
+IMG_inundated=new_IMG_post-new_IMG_pre; % post-pre is inundated area
+
+figure, imshow(imadjust(new_IMG_pre));
+figure, imshow(imadjust(new_IMG_post));
+
+% remove small clusters - less than 100 pxl 
+% 8 here represent adjacent pixel connectivity - those share edges 
+IMG_inundated_final = bwareaopen(IMG_inundated,101,8) ;
+IMG_inundated_final=modefilt(IMG_inundated_final,[3,3]); %mode filter to reduce noise
+IMG_inundated_final = imfill(IMG_inundated_final,8,"holes"); % fills in binary image where 8 specifies connectivity
+
+% display image
+figure, imshow(imadjust(uint8(IMG_inundated_final)));
+figure, imshow(imadjust(inundated_result));
+
+%save images
+imwrite(uint8(IMG_post),strcat('IMG_post_',string(row1),'x',string(col1),'_',string(row2),'x',string(col2),'.tif'),'tif');
+imwrite(uint8(IMG_pre),strcat('IMG_pre_',string(row1),'x',string(col1),'_',string(row2),'x',string(col2),'.tif'),'tif');
+imwrite(new_IMG_post,strcat('water_post_',string(row1),'x',string(col1),'_',string(row2),'x',string(col2),'.tif'),'tif');
+imwrite(new_IMG_pre,strcat('water_pre_',string(row1),'x',string(col1),'_',string(row2),'x',string(col2),'.tif'),'tif');
+imwrite(IMG_inundated_final,strcat('inundated_',string(row1),'x',string(col1),'_',string(row2),'x',string(col2),'.tif'),'tif');
+imwrite(inundated_result,strcat('inundated_result_',string(row1),'x',string(col1),'_',string(row2),'x',string(col2),'.tif'),'tif');
+
+%find area
+pre_area=findArea(new_IMG_pre,r,c)/(10.^6);
+inundated_area=findArea(IMG_inundated_final,r,c)/(10.^6); 
+%find error percentage
+inundated_res_area=findArea(inundated_result,r,c)/(10.^6);
+%write area in file
+s0=strcat("----------Image eastmid----------Size :",string(row1),'x',string(col1),'_',string(row2),'x',string(col2));
+s1=strcat("Total image area: ", num2str(r*c/(10.^4)), " sq km");
+s2=strcat("Natural water body area: ",num2str(pre_area)," sq km");
+s3=strcat("Calculated Inundated area: ",num2str(inundated_area)," sq km");
+s4=strcat("Actual Inundated area: ",num2str(inundated_res_area)," sq km");
+writelines(s0,"v3_1_text_output.txt");
+writelines(s1,"v3_1_text_output.txt",WriteMode="append"); 
+writelines(s2,"v3_1_text_output.txt",WriteMode="append"); 
+writelines(s3,"v3_1_text_output.txt",WriteMode="append");
+writelines(s4,"v3_1_text_output.txt",WriteMode="append"); 
